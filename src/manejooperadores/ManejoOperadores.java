02e1a66133320b3057865d1ce4dddb57961cb142
auyon.j6356 <auyon.j6356@GTZ4LAPJULIOR.allied.global>
2018-03-06 17:20:00 -0600

    New commit

--- src/manejooperadores/ManejoOperadores.java
+++ src/manejooperadores/ManejoOperadores.java
@@ -28,6 +28,68 @@ public class ManejoOperadores {
         System.out.println("d = " +d);
         System.out.println("e = " +e);
         
+        System.out.println("\nOperdor Modulo (residuo): ");
+        System.out.println("x mod 10 = " + a%2);
+        
+        System.out.println("\nOperador Compuesto:");
+        //a = a +4
+        a += 2;
+        b-=4;
+        c*=a;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+        System.out.println("c = " +c);
+        
+        System.out.println("\nOperador Incremento: ");
+        //int a = a +1;
+        //puede reesscribirse como
+        a++;
+        System.out.println("a = " +a);
+        
+        //Preincremento/Decrementa (Se modifica antes de asignar el valor)
+        c = ++a;
+        //Postincremente/Decremento (Se modificar despues de asignar el valor)
+        //La siguiente vez que se evalue b, es cuando se incremente/decrementa
+        d = b++;
+        System.out.println("b = " +b);
+        System.out.println("c = " +c);
+        System.out.println("d = " +d);
+        
+        System.out.println("\nOperador Relacional: ");
+        //a es menor que b
+        boolean res = a < b;
+        System.out.println("res = " + res);
+        
+        System.out.println("\nOperador Ternario: ");
+        int min = (a<b)?a :b ;
+        System.out.println("min = " + min);
+        
+        System.out.println("\nOperador de Asignacion: ");
+        int i, j, k;
+        //cadena de asignacion 
+        i = j = k = 100; // valor de x, y, z igual a 100
+        
+        System.out.println("i = "+i);
+        System.out.println("j = " +j);
+        System.out.println("k = "+k);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
         
         
         
