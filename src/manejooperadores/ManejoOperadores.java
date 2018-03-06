758086242af8feef8ba5eeae474674cb01cd49ea
auyon.j6356 <auyon.j6356@GTZ4LAPJULIOR.allied.global>
2018-03-06 15:03:50 -0600

    New commit

---
+++ src/manejooperadores/ManejoOperadores.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejooperadores;
+
+/**
+ *
+ * @author auyon.j6356
+ */
+public class ManejoOperadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Operadores Aritmeticos: ");
+        int a = 1+2;
+        int b = a*4;
+        int c = b /2;
+        int d = c-a;
+        int e = -d;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+        System.out.println("c = " +c);
+        System.out.println("d = " +d);
+        System.out.println("e = " +e);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
